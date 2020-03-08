32d802b90053cd74448cca96b8d4a5927f88cb1a
sheaffej <sheaffej@gmail.com>
2020-03-08 16:48:20 -0700

    modify run scripts

--- run/imu.sh
+++ run/imu.sh
@@ -10,13 +10,14 @@ PROJ_DIR=$MYDIR/../..  # Directory containing the cloned git repos
 
 DOWNLOADS_DIR=~/Downloads
 
-docker run -it --rm \
+docker run -d --rm \
+--name b2_imu_node
 --net host \
 --privileged \
 --env DISPLAY \
 --env ROS_MASTER_URI=$ROS_MASTER_URI \
 --mount type=bind,source=${DOWNLOADS_DIR},target=/root/Downloads \
 --mount type=bind,source=$PROJ_DIR/b2-imu/b2_imu,target=/ros/src/b2_imu \
-$DOCKER_IMAGE $@
+$DOCKER_IMAGE roslaunch b2_imu minimu9.launch 
 
 
