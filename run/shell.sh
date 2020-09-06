3bb3f1314725fd738f6ae6ced85e0f52eb06902a
sheaffej <sheaffej@gmail.com>
2020-09-05 18:50:13 -0700

    adjust for b2-cli

--- run/shell.sh
+++ run/shell.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
 
 DOCKER_IMAGE="sheaffej/b2-imu"
-CONTAINER_NAME="shell-imu_node"
 LABEL="b2"
-ROS_MASTER_URI=http://docker-server:11311/
 
 MYDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 PROJ_DIR=$MYDIR/../..  # Directory containing the cloned git repos
@@ -12,12 +10,11 @@ PROJ_DIR=$MYDIR/../..  # Directory containing the cloned git repos
 DOWNLOADS_DIR=~/Downloads
 
 docker run -it --rm \
---name ${CONTAINER_NAME} \
 --label ${LABEL} \
 --net host \
 --privileged \
 --env DISPLAY \
---env ROS_MASTER_URI=$ROS_MASTER_URI \
+--env ROS_MASTER_URI \
 --mount type=bind,source=${DOWNLOADS_DIR},target=/root/Downloads \
 --mount type=bind,source=$PROJ_DIR/b2-imu/b2_imu,target=/ros/src/b2_imu \
 $DOCKER_IMAGE $@
